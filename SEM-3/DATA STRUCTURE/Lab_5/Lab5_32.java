d2265e73f5732ced9250c7266bdbb9665097076a
RudraC-07 <rudrajchavda05@gmail.com>
2024-06-27 13:32:24 +0530

    Added

--- SEM-3/DATA STRUCTURE/Lab_5/Lab5_32.java
+++ SEM-3/DATA STRUCTURE/Lab_5/Lab5_32.java
@@ -1,39 +1,56 @@
-import java.util.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
 
 public class Lab5_32 {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    System.out.print("Enter size of array : ");
-    int n = sc.nextInt();
-    String [] a = new String[n];
+        System.out.println("enter size of arrray of string");
+        int n= sc.nextInt();
 
-    for(int i=0;i<n;i++){
-      System.out.print("Enter elements at ["+i+"] : ");
-      a[i] = sc.next();
-    }
+        String[] a = new String[n];
 
-    int number = (int)(Math.random()*(n));
-
-    System.out.println("Word = "+a[number]);
-
-    System.out.print("Enter a Word : ");
-    String word = sc.next();
-    int x = 0;
-
-    for(int i=0;i<a[number].length();i++){
-      for(int j=0;j<a[number].length();j++){
-        if(word.charAt(0)==a[number].charAt(0)){
-          x++;
+        for(int i=0;i<a.length;i++){
+            System.out.println("enter a["+i+"]");
+            a[i] = sc.next();
         }
-      }
-    }
+        int p = (int)((n-1)*Math.random());
 
-    if(x==1){
-      System.out.println("Anagram");
+        System.out.println(p);
+
+        
+        char [] c = a[p].toCharArray();
+
+        Arrays.sort(c);
+
+        String s1 = new String();
+        System.out.println("enter your string to anagram");
+        s1 = sc.next();
+
+        char [] c1 = s1.toCharArray();
+
+        Arrays.sort(c1);
+
+        if(s1.length()==a[p].length()){
+            for(int i=0;i<s1.length();i++){
+                if(c[i]==c1[i]){
+
+                }
+                else{
+                    System.out.println("this is not analog");
+                    return;
+                }
+            }
+        }
+
+        else{
+            System.out.println("this is not analog");
+            return;
+        }
+
+        System.out.println(s1+"is the is a analog of "+a[p]);
+
     }
-    else{
-      System.out.println("not");
-    }
-  }
 }
