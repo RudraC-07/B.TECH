67dc60d20c8fd5934ca5ac726eeea5dba6d9f55b
RudraC-07 <rudrajchavda05@gmail.com>
2024-07-17 08:07:42 +0530

    Added

--- SEM-3/DBMS-I/LAB_7.sql
+++ SEM-3/DBMS-I/LAB_7.sql
@@ -53,14 +53,69 @@ SELECT SUM(SALARY) AS TOTAL_SAL FROM EMP
 WHERE DEPARTMENT = 'HR'
 
 --9. Count total number of cities of employee without duplication.
+SELECT COUNT(DISTINCT CITY) AS COUNT FROM EMP
+
 --10. Count unique departments.
+SELECT COUNT(DISTINCT DEPARTMENT) AS COUNT FROM EMP 
+
 --11. Give minimum salary of employee who belongs to Ahmedabad.
 SELECT MIN(SALARY) AS MIN_SAL FROM EMP
 WHERE CITY = 'AHMEDABAD'
 
 --12. Find city wise highest salary.
+SELECT CITY,MAX(SALARY) AS MAX_SAL FROM EMP
+GROUP BY CITY
+
 --13. Find department wise lowest salary.
+SELECT DEPARTMENT,MIN(SALARY) AS MIN_SAL FROM EMP
+GROUP BY DEPARTMENT
+
 --14. Display city with the total number of employees belonging to each city.
+SELECT CITY,COUNT(EID) AS TOTAL_EMP FROM EMP
+GROUP BY CITY
+
 --15. Give total salary of each department of EMP table.
+SELECT DEPARTMENT,SUM(SALARY) AS TOTAL_SAL FROM EMP
+GROUP BY DEPARTMENT
+
 --16. Give average salary of each department of EMP table without displaying the respective department
 --name.
+SELECT AVG(SALARY) AS AVG_SAL FROM EMP
+GROUP BY DEPARTMENT
+
+
+----------PART-B (AGGREGATE FUNCTION)----------
+
+
+--1. Count the number of employees living in Rajkot.
+SELECT CITY,COUNT(EID) AS EMP_COUNT FROM EMP
+WHERE CITY = 'RAJKOT'
+GROUP BY CITY
+
+--2. Display the difference between the highest and lowest salaries. Label the column DIFFERENCE.
+SELECT MAX(SALARY)-MIN(SALARY) AS DIFFERENCE FROM EMP
+
+--3. Display the total number of employees hired before 1st January, 1991.
+SELECT COUNT(EID) AS EMP_COUNT FROM EMP
+WHERE JoiningDate < '1991-01-01'
+
+
+----------PART-C (AGGREGATE FUNCTION)----------
+
+
+--1. Count the number of employees living in Rajkot or Baroda.
+SELECT COUNT(EID) AS EMP_COUNT FROM EMP
+WHERE CITY = 'RAJKOT' OR CITY = 'BARODA'
+
+--2. Display the total number of employees hired before 1st January, 1991 in IT department.\
+SELECT COUNT(EID) AS EMP_COUNT FROM EMP
+WHERE JoiningDate < '1991-01-01' AND DEPARTMENT = 'IT'
+
+--3. Find the Joining Date wise Total Salaries.
+SELECT JoiningDate,SUM(SALARY) AS TOTAL_SAL FROM EMP
+GROUP BY JoiningDate
+
+--4. Find the Maximum salary department & city wise in which city name starts with ‘R’.
+SELECT DEPARTMENT,CITY,MAX(SALARY) AS MAX_SAL FROM EMP
+WHERE CITY LIKE 'R%'
+GROUP BY DEPARTMENT,CITY
---
+++ SEM-3/DBMS-I/LAB_8.sql
@@ -0,0 +1,58 @@
+CREATE TABLE SALES_DATA(
+	REGION VARCHAR(20),
+	PRODUCT VARCHAR(20),
+	SALES_AMOUNT INT,
+	YEAR INT
+);
+
+INSERT INTO SALES_DATA VALUES
+('NORTH AMERICA','WATCH',1500,2023),
+('EUROPE','MOBILE',1200,2023),
+('ASIA','WATCH',1800,2023),
+('NORTH AMERICA','TV',900,2024),
+('EUROPE','WATCH',2000,2024),
+('ASIA','MOBILE',1000,2024),
+('NORTH AMERICA','MOBILE',1600,2023),
+('EUROPE','TV',1500,2023),
+('ASIA','TV',1100,2024),
+('NORTH AMERICA','WATCH',1700,2024);
+
+--SELECT * FROM SALES_DATA
+
+
+----------PART-A (GROUP BY & ORDER BY)----------
+
+
+--1. Display Total Sales Amount by Region.
+SELECT REGION,SUM(SALES_AMOUNT) AS TOTAL_AMOUNT FROM SALES_DATA
+GROUP BY REGION
+
+--2. Display Average Sales Amount by Product
+SELECT PRODUCT,AVG(SALES_AMOUNT) AS AVG_AMOUNT FROM SALES_DATA
+GROUP BY PRODUCT
+
+--3. Display Maximum Sales Amount by Year
+SELECT YEAR,MAX(SALES_AMOUNT) AS MAX_AMOUNT FROM SALES_DATA
+GROUP BY YEAR
+
+--4. Display Minimum Sales Amount by Region and Year
+SELECT REGION,YEAR,MIN(SALES_AMOUNT) AS MIN_AMOUNT FROM SALES_DATA
+GROUP BY REGION,YEAR
+
+--5. Count of Products Sold by Region
+SELECT REGION,COUNT(PRODUCT) AS PDT_COUNT FROM SALES_DATA
+GROUP BY REGION 
+
+--6. Display Sales Amount by Year and Product
+SELECT YEAR,PRODUCT,SUM(SALES_AMOUNT) FROM SALES_DATA
+GROUP BY YEAR,PRODUCT
+
+--7. Display Regions with Total Sales Greater Than 5000
+--SELECT REGION FROM SALES_DATA
+--WHERE SUM(SALES_AMOUNT) > 5000
+
+--8. Display Products with Average Sales Less Than 10000
+--9. Display Years with Maximum Sales Exceeding 500
+--10. Display Regions with at Least 3 Distinct Products Sold.
+--11. Display Years with Minimum Sales Less Than 1000
+--12. Display Total Sales Amount by Region for Year 2023, Sorted by Total Amount
